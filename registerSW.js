4d6b48e23b7378dbab7590ac828732e7aa13c67f
rpdil23 <rpdil23@users.noreply.github.com>
2025-06-17 09:07:42 +0000

    deploy: e4329c994932f53a06666a2df339bda0ed90b50c

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/marriage-biodata-generator/sw.js', { scope: '/marriage-biodata-generator/' })})}
